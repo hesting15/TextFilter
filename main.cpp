6eb10aa5f426737b00fe8be95996e43d63fb4eca
Stepan Sypliak <hesting15@gmail.com>
2020-02-18 16:06:57 +0200

    Display application icon in kubuntu

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
-//#include <Windows.h>
 
 int main(int argc, char *argv[])
 {
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-
     QApplication a(argc, argv);
+
     MainWindow w;
+    w.setWindowIcon(QIcon(":/Icon64.png"));
     w.show();
 
     return a.exec();
